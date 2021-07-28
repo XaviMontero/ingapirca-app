780ac1314c7e99049001865d15ab887aa41fd8e4
WilyPrLeon <58718308+WilyPrLeon@users.noreply.github.com>
2021-07-27 23:09:39 -0500

    cambio

--- lib/util/lugares.dart
+++ lib/util/lugares.dart
@@ -7,7 +7,7 @@ List lugares = [
   },
   {
     "img": "assets/cabezas.jpeg",
-    "title": "Las cabezas-clava",
+    "title": "Cerámica Cañari",
     "short": "Las cabezas-clava son piezas trabajadas en arenisca",
     "description": "Las cabezas-clava son piezas trabajadas en arenisca (30 a 45 cm de longitud) con cabeza zoomorfa tallada en alto relieve en la que se destacan representaciones de aves, auquénidos y felinos. Estos monolitos se han encontrado en Ingapirca asociados a las estructuras arquitectónicas con ocupación cañari: Pilaloma, La Condamine, y en ínfima cantidad en la elipse- La forma angular de las piezas sugiere una difícil inserción en los muros, y en la medida que no han sido encontradas in situ, no se puede imaginar si se colocaba fuera o adentro de las estructuras arquitectónicas. Sin embargo, se podría afirmar con certeza, que generalmente están asociados a áreas de entierros, como marcadores de tumbas" 
   },
