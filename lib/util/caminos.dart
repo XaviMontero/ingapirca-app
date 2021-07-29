ebd8125fd249469c435ab673c735eb14580e5382
WilyPrLeon <58718308+WilyPrLeon@users.noreply.github.com>
2021-07-28 21:56:37 -0500

    cmabios en caminos y agregar imagenes

--- lib/util/caminos.dart
+++ lib/util/caminos.dart
@@ -2,24 +2,45 @@ import 'package:flutter/material.dart';
 
 List caminos = [
   {
-    "name": "Azuay",
+    "name": "Tres Cruces",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/food1.jpeg",
-    "description": "lo que saque"
+    "img": "assets/2.JPG",
+    "description": "Este adoratorio se encuentra a 4.350 m.s.n.m., en el punto mas alto del camino inca en el Ecuador. Se trata de un amontonamiento de piedras de 3m. de alto junto a tres estructuras rectangulares. Las piedras son las ofrendas que hacían los viajeros a lo apus para asegurarse una feliz travesía por las montañas. El apelativo de Tres Cruces es colonial y hace alusión a la presencia de cruces marcadoras de las tumbas de viajeros."
   },
   {
-    "name": "Azuay",
+    "name": "Culebrillas",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/food1.jpeg",
-    "description": "lo que saque"
+    "img": "assets/3.JPG",
+    "description": "La historia oral de la región señala a este lugar (3.885 m.s.n.m.) como el escenario donde se recrea un mito de origen de los cañaris, según el cual una enorme culebra progenitora había bajado de la montaña para ahogarse en la laguna que fue adoptado por los incas, a cuyo efecto dotaron al lugar con una serie de construcciones."
   },
   {
-    "name": "Azuay",
+    "name": "Labrashca",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/food1.jpeg",
-    "description": "lo que saque"
+    "img": "assets/4.JPG",
+    "description": "Es un campo de piedras almohadillas, algunas de gran tamaño, dispersas en amplio terreno, cerca de la laguna de Culebrillas. Hay dos interpretaciones: tal vez fue una cantera de aprovisionamiento de material pétreo; o en ese lugar se dispuso el material trabajado para la construcción de un edificio in situ. En cualquiera de los dos casos, hay procesamiento de la materia prima, actividad que probablemente genero el vocablo aglutinado “labrashcarumi” o “piedra labrada”."
+  },
+  {
+    "name": "Paredones",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/5.JPG",
+    "description": "El Tambu en si es un edificio de cinco cuartos, con una “cocina” añadida por los viajeros de la colonia y la república. El sitio se encuentra en posición estratégica, junto al Qhapaq Ñan, para atender a cualquier viajero en travesía por el gran macizo del Azuay. Además, al otro lado del Qhapaq Ñan, hay un conjunto de estructuras pequeñas alineadas con el camino inca, cuya existencia nos permite inferir que tal vez fue, centro de acopio de vituallas y armas, estación de chaskis y lugar de descanso para ejércitos en marcha."
+  },
+  {
+    "name": "Puyal",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/6.JPG",
+    "description": " En dirección norte-sur, el Qhapaq Ñan asoma por el filo del macizo azuayo en Quilloloma y baja el valle glaciar de Culebrillas, recorriéndolo por un costado de la laguna hasta llegar a Paredones, enfila luego hacia la llanura de Puyal (3.800 m.s.n.m.) y descendiendo a Ingapirca. Puyal este sujeto a nevadas y fuertes vientos que dificultan la travesía. Los incas hicieron un excelente trabajo en la construcción de un camino firme (10 a 14 m. de ancho) que se mantiene en buen estado de conservación."
+  },
+  {
+    "name": "Wuasipamba",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/7.JPG",
+    "description": "Continuando por el Puyal, a 5 Km. de Paredones se encuentra Wuasipamba (3.600 m.s.n.m.) que estaba dotado de tres habitaciones pequeñas, cuya función era la de chaskiwasi. Aquí se albergaban los hombres correos o chaskis que facilitaban la comunicación en todo el imperio Inca."
   },
 ];
