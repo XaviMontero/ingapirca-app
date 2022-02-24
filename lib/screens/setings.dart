d97c2cf22cddc3bc4dca8a092371e1ced9334235
XaviMontero <oxmonteroc83@est.ucacue.edu.ec>
2022-02-23 21:26:13 -0500

    :green_apple: add new view

--- lib/screens/setings.dart
+++ lib/screens/setings.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:ingapirca_app/util/preference.dart';
 
 class Seting extends StatelessWidget {
-  @override
+@override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Preferencias'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Preferences'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(
+          5.0,
+        ),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(
+            preferences.length,
+            (index) {
+              var cat = preferences[index];
+              return Container(
+                padding: EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.asset(
+                        cat["img"],
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.height,
+                        fit: BoxFit.cover,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            // Add one stop for each color. Stops should increase from 0 to 1
+                            stops: [0.2, 0.7],
+                            colors: [
+                              cat['color1'],
+                              cat['color2'],
+                            ],
+                            // stops: [0.0, 0.1],
+                          ),
+                        ),
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.height,
+                      ),
+                      Center(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.height,
+                          padding: const EdgeInsets.all(1),
+                          // constraints: BoxConstraints(
+                          //   minWidth: 20,
+                          //   minHeight: 20,
+                          // ),
+                          child: Center(
+                            child: Text(
+                              cat["name"],
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Icon(Icons.navigate_next),
+        backgroundColor: Theme.of(context).accentColor,
+      ),
     );
   }
 }
+
+ 
---
+++ lib/util/preference.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+List preferences = [
+  {
+    "name": "Guerra",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Guerra2.jpg"
+  },
+  {
+    "name": "Costumbre",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Costumbres.jpg"
+  },
+  {
+    "name": "Alimento",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Alimento.jpg"
+  },
+  {
+    "name": "Funeraria",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Funeraria.jpg"
+  },
+  {
+    "name": "Herramientas",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Herramientas.jpg"
+  },
+  {
+    "name": "Historia",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Historia.jpg"
+  },
+  {
+    "name": "Vestimenta",
+    "color1": Color.fromARGB(100, 0, 0, 0),
+    "color2": Color.fromARGB(100, 0, 0, 0),
+    "img": "assets/Vestimenta.jpg"
+  },
+];
